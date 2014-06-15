1d25ba61634b4005fcba51954ec9bf5c01ba661d
Dipesh Prajapati <pdipesh76@gmail.com>
2014-06-15 22:13:31 +0530

    added SMTP setting to Prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ Test::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {  
+      :address              => "smtp.gmail.com",  
+      :port                 => 587,  
+      :domain               => "gmail.com",  
+      :user_name            => "pdipesh87", #Your user name
+      :password             => "dipu8787", # Your password
+      :authentication       => "plain",  
+      :enable_starttls_auto => true  
+   }
 end
